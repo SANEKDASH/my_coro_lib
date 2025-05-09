b1b3678132fda715f532952f651e6c7217e8c476
sanekdash <dashinsky_alexander92@mail.ru>
2025-05-09 12:22:43 +0300

    + my_coro_init
    + my_coro_destroy

---
+++ my_coro.c
@@ -0,0 +1,36 @@
+#include "my_coro.h"
+
+int my_coro_init(struct my_coro *c, void (*func)(void *), void *arg)
+{
+  c->func = func;
+  c->arg  = arg;
+  
+  if (getcontext(&c->routine_ctx) < 0) {
+	perror("failed to get context");
+	return -1;
+  }
+
+  c->routine_ctx.uc_stack.ss_sp = (char *) malloc(DEFAULT_CORO_STACK_SIZE);
+
+  if (c->routine_ctx.uc_stack.ss_sp == NULL) {
+	perror("failed to alloc memory");
+	return -1;
+  }
+  
+  c->routine_ctx.uc_stack.ss_size = DEFAULT_CORO_STACK_SIZE;
+  c->routine_ctx.uc_link = &c->swap_ctx;
+
+  makecontext(&c->routine_ctx, (void (*)(void)) c->func, 1, c->arg);
+  
+  return 0;
+}
+
+int my_coro_destroy(struct my_coro *c)
+{
+  free(c->routine_ctx.uc_stack.ss_sp);
+
+  c->func = NULL;
+  c->arg = NULL;  
+
+  return 0;
+}
---
+++ my_coro.h
@@ -0,0 +1,30 @@
+#ifndef MY_CORO_LIB_HEADER
+#define MY_CORO_LIB_HEADER
+
+#include <ucontext.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef enum {
+  CORO_RUN,
+  CORO_YILDED,
+  CORO_WAIT,
+  CORO_FINISH,
+} my_coro_state_t;
+
+struct my_coro {
+  ucontext_t routine_ctx;
+  ucontext_t swap_ctx;
+  my_coro_state_t state;
+
+  void (*func)(void *);
+
+  void *arg;
+};
+
+static const size_t DEFAULT_CORO_STACK_SIZE = 1024 * 16; // bytes 
+
+int my_coro_init   (struct my_coro *c, void (*func)(void *), void *arg);
+int my_coro_destroy(struct my_coro *c);
+
+#endif
